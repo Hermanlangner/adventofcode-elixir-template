76b5358cc4cf33850a140b136162d1f20e9aa325
Herman Langner <hermandlangner@gmail.com>
2025-12-04 11:40:24 +0200

    part 1 solved

--- lib/advent_of_code/solution/year_2025/day_01.ex
+++ lib/advent_of_code/solution/year_2025/day_01.ex
@@ -5,6 +5,15 @@ defmodule AdventOfCode.Solution.Year2025.Day01 do
   defp p1_upper_limit, do: 99
   defp p1_starting_point, do: 50
 
+  defp transition_position(:left), do: p1_upper_limit()
+  defp transition_position(:right), do: p1_lower_limit()
+
+  defp ticks_until_transition(:left, pos), do: pos + 1
+  defp ticks_until_transition(:right, pos), do: p1_upper_limit() - pos + 1
+
+  defp set_position(:left, pos, rotations), do: pos - rotations
+  defp set_position(:right, pos, rotations), do: pos + rotations
+
   defp p1_parse_input(input) do
     input
     |> String.split("\n")
@@ -16,130 +25,31 @@ defmodule AdventOfCode.Solution.Year2025.Day01 do
   end
 
   def part1(input) do
-    {pw, _final_pos} =
+    {password, _final_position} =
       p1_parse_input(input)
-      |> Enum.reduce({0, p1_starting_point()}, fn rotation, {pw, position} ->
-        new_position = rotate_dial(position, rotation)
+      |> Enum.reduce({0, p1_starting_point()}, &rotate_dial(&1, &2))
 
-        case new_position do
-          0 -> {pw + 1, new_position}
-          _other -> {pw, new_position}
-        end
-      end)
-
-    pw
+    password
   end
 
-  defp rotate_dial(cp, {d, r}) when cp < 0,
-    do: raise("Current Position is less than 0, #{cp} with dir: #{to_string(d)}, rot: #{r}")
+  defp rotate_dial(rotation, {password, position}) do
+    new_position = apply_rotation(position, rotation)
 
-  defp rotate_dial(0, {:right, 0}), do: 0
-
-  defp rotate_dial(0, {:right, rotations}) do
-    ticks_until_limit = p1_upper_limit() + 1
-
-    cond do
-      rotations < ticks_until_limit ->
-        IO.puts(
-          "Rotations from upper limit, does not exceed new cycle with #{rotations}, returning #{0 + rotations}"
-        )
-
-        0 + rotations
-
-      true ->
-        IO.puts(
-          "Right Rotation: current_position: #{0}, rotations #{rotations} exceeded limit #{100} so continueing upper limit with rotations at #{rotations - 100}"
-        )
-
-        rotate_dial(p1_lower_limit(), {:right, rotations - ticks_until_limit})
+    case new_position do
+      0 -> {password + 1, new_position}
+      _other -> {password, new_position}
     end
   end
 
-  defp rotate_dial(current_position, {:right, rotations}) do
-    ticks_until_upper_limit = p1_upper_limit() - current_position + 1
-    IO.puts("Right limit #{ticks_until_upper_limit}")
+  defp apply_rotation(current_position, {_dir, 0}), do: current_position
 
-    cond do
-      rotations < ticks_until_upper_limit ->
-        IO.puts(
-          "Right Rotation: current position: #{current_position} Fewer rotations #{rotations} than limit #{ticks_until_upper_limit} so returning: #{current_position - rotations}"
-        )
+  defp apply_rotation(current_position, {dir, rotations}) do
+    ticks_until_transition = ticks_until_transition(dir, current_position)
 
-        current_position + rotations
-
-      ticks_until_upper_limit == 1 ->
-        IO.puts(
-          "Right Rotation: Fewer rotations #{rotations} than limit #{ticks_until_upper_limit} so returning: #{current_position - rotations}"
-        )
-
-        rotate_dial(p1_lower_limit(), {:right, rotations - ticks_until_upper_limit})
-
-      true ->
-        IO.puts(
-          "Right Rotation: current_position: #{current_position}, rotations #{rotations} exceeded limit #{ticks_until_upper_limit} so continueing upper limit with rotations at #{rotations - ticks_until_upper_limit}"
-        )
-
-        rotate_dial(p1_lower_limit(), {:right, rotations - ticks_until_upper_limit})
-    end
-  end
-
-  defp rotate_dial(99, {:left, 0}), do: 99
-
-  defp rotate_dial(99, {:left, rotations}) do
-    ticks_until_limit = p1_upper_limit() + 1
-
-    cond do
-      rotations < ticks_until_limit ->
-        IO.puts(
-          "Rotations from upper limit, does not exceed new cycle with #{rotations}, returning #{99 - rotations}"
-        )
-
-        99 - rotations
-
-      true ->
-        IO.puts(
-          "Left Rotation: current_position: #{99}, rotations #{rotations} exceeded limit #{100} so continueing upper limit with rotations at #{rotations - 100}"
-        )
-
-        rotate_dial(p1_upper_limit(), {:left, rotations - ticks_until_limit})
-    end
-  end
-
-  defp rotate_dial(current_position, {:left, rotations} = left_rota) do
-    ticks_until_upper_limit = current_position + 1
-
-    cond do
-      rotations < ticks_until_upper_limit ->
-        IO.puts(
-          "Left Rotation: current position: #{current_position} Fewer rotations #{rotations} than limit #{ticks_until_upper_limit} so returning: #{current_position - rotations}"
-        )
-
-        current_position - rotations
-
-      ticks_until_upper_limit == 1 ->
-        IO.puts(
-          "Left Rotation: Fewer rotations #{rotations} than limit #{ticks_until_upper_limit} so returning: #{current_position - rotations}"
-        )
-
-        rotate_dial(p1_upper_limit(), {:left, rotations - ticks_until_upper_limit})
-
-      true ->
-        IO.puts(
-          "Left Rotation: current_position: #{current_position}, rotations #{rotations} exceeded limit #{ticks_until_upper_limit} so continueing upper limit with rotations at #{rotations - ticks_until_upper_limit}"
-        )
-
-        rotate_dial(p1_upper_limit(), {:left, rotations - ticks_until_upper_limit})
-    end
-  end
-
-  defp apply_rotation(current_position, {direction, rotations}, limit) do
-    if rotations <= limit do
-      IO.puts("Returning #{current_position - rotations}")
-      current_position - rotations
+    if rotations < ticks_until_transition do
+      set_position(dir, current_position, rotations)
     else
-      leftover_rotations = rotations - limit
-      IO.puts("#{to_string(direction)} Leftovers #{leftover_rotations}")
-      rotate_dial(p1_lower_limit(), {direction, leftover_rotations})
+      apply_rotation(transition_position(dir), {dir, rotations - ticks_until_transition})
     end
   end
 
